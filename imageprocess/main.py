a5a62e50eae2aebea6670d6f2d3ef1275e123bf4
MJKim <banwonkim@naver.com>
2025-12-09 20:56:45 +0900

    Add files via upload

---
+++ imageprocess/main.py
@@ -0,0 +1,17 @@
+'''
+뭔 소린지 모르겠다
+69:#REC 모드 켜짐
+79:녹화 저장
+470:labeling 버튼 함수
+506 : REC (recording) 버튼 함수
+522:display_video_stream : 큐에서 이미지 뽑아서 화면 출력
+
+'''
+
+
+
+
+
+
+import sys
+from PySide6.QtWidgets import QApplication, QPushButton
